adb9405701c17af4b29f75d01f7e4dc8e8625095
liukun <451564319@qq.com>
2019-03-05 22:05:49 +0800

    fucing serde

--- src/main/java/edu/tsinghua/k1/TimeSeriesMap.java
+++ src/main/java/edu/tsinghua/k1/TimeSeriesMap.java
@@ -1,7 +1,7 @@
 package edu.tsinghua.k1;
 
-import com.sun.xml.internal.messaging.saaj.util.ByteInputStream;
-import com.sun.xml.internal.messaging.saaj.util.ByteOutputStream;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -42,13 +42,13 @@ public class TimeSeriesMap {
   public void serialize(File file) throws IOException {
     try (RandomAccessFile reader = new RandomAccessFile(file, "rw")) {
       reader.seek(4);
-      ByteOutputStream outputStream = new ByteOutputStream();
+      ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
       ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
       objectOutputStream.writeObject(this.timeSeriesToUID);
       for (Map.Entry<String, Integer> entry : this.timeSeriesToUID.entrySet()) {
         System.out.println("ser2.0 key value: " + entry.getKey() + " " + entry.getValue());
       }
-      reader.write(outputStream.getBytes());
+      reader.write(outputStream.toByteArray());
     }
   }
 
@@ -60,7 +60,7 @@ public class TimeSeriesMap {
         int size = reader.readInt();
         byte[] bytes = new byte[(int) (reader.length() - 4)];
         reader.readFully(bytes);
-        ByteInputStream inputStream = new ByteInputStream(bytes, bytes.length);
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(bytes);
         ObjectInputStream ObjectInputStream = new ObjectInputStream(inputStream);
         this.timeSeriesToUID = (ConcurrentHashMap<String, Integer>) ObjectInputStream.readObject();
         for (Map.Entry<String, Integer> entry : this.timeSeriesToUID.entrySet()) {
