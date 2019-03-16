14c18dc55c98e7a4f158c22234fc1a8ecc2e4965
liukun <451564319@qq.com>
2019-03-16 14:10:18 +0800

    change the configuration

--- src/main/java/edu/tsinghua/k1/rocksdb/RocksDBTimeSeriesDBFactory.java
+++ src/main/java/edu/tsinghua/k1/rocksdb/RocksDBTimeSeriesDBFactory.java
@@ -34,11 +34,11 @@ public class RocksDBTimeSeriesDBFactory implements ITimeSeriesDBFactory {
     rocksOptions.setWriteBufferSize(128 << 20);
     rocksOptions.setMaxWriteBufferNumber(2);
     rocksOptions.setMaxBackgroundFlushes(1);
-    rocksOptions.setBaseBackgroundCompactions(1);
+    rocksOptions.setBaseBackgroundCompactions(4);
     rocksOptions.setMinWriteBufferNumberToMerge(1);
     rocksOptions.setTargetFileSizeBase(32 << 20);
-    rocksOptions.setLevel0FileNumCompactionTrigger(4);
-    rocksOptions.setMaxBytesForLevelBase(512 << 20);
+    rocksOptions.setLevel0FileNumCompactionTrigger(2);
+    rocksOptions.setMaxBytesForLevelBase(256 << 20);
     rocksOptions.setMaxOpenFiles(1048576);
     rocksOptions.setWalDir("/data2/rocksdbwal");
     try {
