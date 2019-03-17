8868f5d79ff3c9d3b6aa3b6d006abf56d960176b
liukun <451564319@qq.com>
2019-03-17 14:41:46 +0800

    chagne to 32m

--- src/main/java/edu/tsinghua/k1/rocksdb/RocksDBTimeSeriesDBFactory.java
+++ src/main/java/edu/tsinghua/k1/rocksdb/RocksDBTimeSeriesDBFactory.java
@@ -31,7 +31,7 @@ public class RocksDBTimeSeriesDBFactory implements ITimeSeriesDBFactory {
     ITimeSeriesDB db;
     Options rocksOptions = (Options) options;
     // 128M memtable and l0 sstable
-    rocksOptions.setWriteBufferSize( 4<< 30);
+    rocksOptions.setWriteBufferSize( 32<< 20);
     rocksOptions.setMaxWriteBufferNumber(2);
     rocksOptions.setMaxBackgroundFlushes(1);
     rocksOptions.setBaseBackgroundCompactions(4);
