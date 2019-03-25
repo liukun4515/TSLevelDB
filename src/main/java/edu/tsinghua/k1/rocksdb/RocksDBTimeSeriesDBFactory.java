1c909f52d1e4fa5d7da86cf0858bf1629af82542
liukun <451564319@qq.com>
2019-03-25 15:13:49 +0800

    add stat

--- src/main/java/edu/tsinghua/k1/rocksdb/RocksDBTimeSeriesDBFactory.java
+++ src/main/java/edu/tsinghua/k1/rocksdb/RocksDBTimeSeriesDBFactory.java
@@ -4,9 +4,13 @@ import edu.tsinghua.k1.api.ITimeSeriesDB;
 import edu.tsinghua.k1.api.ITimeSeriesDBFactory;
 import java.io.File;
 import java.io.IOException;
+import org.rocksdb.DBOptions;
+import org.rocksdb.InfoLogLevel;
+import org.rocksdb.Logger;
 import org.rocksdb.Options;
 import org.rocksdb.RocksDB;
 import org.rocksdb.RocksDBException;
+import org.rocksdb.Statistics;
 
 /**
  * Created by liukun on 19/3/12.
@@ -15,6 +19,22 @@ public class RocksDBTimeSeriesDBFactory implements ITimeSeriesDBFactory {
 
   static {
     RocksDB.loadLibrary();
+  }
+
+  private static class MyLogger extends Logger {
+
+    public MyLogger(Options options) {
+      super(options);
+    }
+
+    public MyLogger(DBOptions dboptions) {
+      super(dboptions);
+    }
+
+    @Override
+    protected void log(InfoLogLevel infoLogLevel, String logMsg) {
+      System.out.println("Rocksdb Logger: " + logMsg);
+    }
   }
 
   private static class Holder {
@@ -31,17 +51,23 @@ public class RocksDBTimeSeriesDBFactory implements ITimeSeriesDBFactory {
     ITimeSeriesDB db;
     Options rocksOptions = (Options) options;
     // 128M memtable and l0 sstable
-//    rocksOptions.setWriteBufferSize(128 << 20);
-//    rocksOptions.setMaxWriteBufferNumber(2);
-//    rocksOptions.setMaxBackgroundFlushes(1);
-//    rocksOptions.setBaseBackgroundCompactions(1);
-//    rocksOptions.setMinWriteBufferNumberToMerge(1);
-//    rocksOptions.setTargetFileSizeBase(128 << 20);
-//    rocksOptions.setLevel0FileNumCompactionTrigger(4);
-//    rocksOptions.setMaxBytesForLevelBase(512 << 20);
-//    rocksOptions.setMaxOpenFiles(1048576);
-//    rocksOptions.setReportBgIoStats(true);
-//    rocksOptions.setWalDir("/data2/rocksdbwal");
+    rocksOptions.setWriteBufferSize(128 << 20);
+    rocksOptions.setMaxWriteBufferNumber(2);
+    rocksOptions.setMaxBackgroundFlushes(1);
+    rocksOptions.setBaseBackgroundCompactions(1);
+    rocksOptions.setMinWriteBufferNumberToMerge(1);
+    rocksOptions.setTargetFileSizeBase(128 << 20);
+    rocksOptions.setLevel0FileNumCompactionTrigger(4);
+    rocksOptions.setMaxBytesForLevelBase(512 << 20);
+    rocksOptions.setMaxOpenFiles(1048576);
+    rocksOptions.setReportBgIoStats(true);
+    rocksOptions.setWalDir("/data2/rocksdbwal");
+    // set stat
+    rocksOptions.setStatistics(new Statistics());
+    rocksOptions.setStatsDumpPeriodSec(60);
+    // set logger
+    rocksOptions.setLogger(new MyLogger(rocksOptions));
+
     try {
       RocksDB rocksDB = RocksDB.open((org.rocksdb.Options) options, path.getPath());
       db = new RocksDBTimeSeriesDB(path, rocksDB);
